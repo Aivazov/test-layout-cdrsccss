62bbd111381306ebb09e8f6ba0599edf5733acd4
Tim <ivazow.t@gmail.com>
2026-01-14 15:10:26 +0200

    adding hero person image and some fixes

--- src/components/hero/HeroSection.jsx
+++ src/components/hero/HeroSection.jsx
@@ -1,22 +1,24 @@
-import { Box, Button, Stack, CardMedia, Divider } from "@mui/material";
-import { heroSectionContainer, heroSectionContentBox, heroSectionOverlay } from "./heroSectionStyles";
-import Waves from "../../UI/Assets/Hero/Waves";
-import HeroSectionTitle from "../../UI/components/HeroSection/HeroSectionTitle";
-import HeroSectionParagraph from "../../UI/components/HeroSection/HeroSectionParagraph";
-import AppearingAnimation from "../../UI/Animations/AppearingAnimation";
-import SwipingFromLeftAnimation from "../../UI/Animations/SwipingFromLeftAnimation";
+import { Box, Button, Stack, CardMedia, Divider } from '@mui/material';
+import {
+  heroSectionContainer,
+  heroSectionContentBox,
+  heroSectionOverlay,
+} from './heroSectionStyles';
+import Waves from '../../UI/Assets/Hero/Waves';
+import HeroSectionTitle from '../../UI/components/HeroSection/HeroSectionTitle';
+import HeroSectionParagraph from '../../UI/components/HeroSection/HeroSectionParagraph';
+import AppearingAnimation from '../../UI/Animations/AppearingAnimation';
+import SwipingFromLeftAnimation from '../../UI/Animations/SwipingFromLeftAnimation';
 
 const HeroSection = () => {
   return (
-    <Box component="section" sx={heroSectionContainer} >
-
+    <Box component='section' sx={heroSectionContainer}>
       {/* Overlay */}
       <Box sx={heroSectionOverlay} />
 
       {/* Content */}
-      <Box sx={heroSectionContentBox} >
+      <Box sx={heroSectionContentBox}>
         <Box>
-
           <AppearingAnimation delay={0.9}>
             <HeroSectionTitle />
           </AppearingAnimation>
@@ -26,64 +28,71 @@ const HeroSection = () => {
           </AppearingAnimation>
 
           <AppearingAnimation delay={1}>
-            <Divider sx={{ my: 1.5, borderColor: "rgba(255,255,255,0.4)", maxWidth: '360px' }} />
-          </AppearingAnimation>
-
-
-          <Stack
-            direction={{ xs: "column", sm: "row" }}
-            spacing={3}
-            mt={5}
-          >
-          <AppearingAnimation delay={1.1}>
-            <Button
-              variant="contained"
-              size="large"
-              sx={{ px: 4, py: 1.5, fontWeight: "bold" }}
-            >
-              Request Service
-            </Button>
-          </AppearingAnimation>
-            
-          <AppearingAnimation delay={1.2}>
-            <Button
-              variant="outlined"
-              component="a"
-              href="tel:+1234567890"
-              size="large"
+            <Divider
               sx={{
-                color: "#fff",
-                borderColor: "#fff",
-                // px: 4,
-                py: 1.5,
-                fontWeight: "bold",
-                "&:hover": {
-                  backgroundColor: "rgba(255, 255, 255, 0.1)",
-                  borderColor: "#fff",
-                },
+                my: 1.5,
+                borderColor: 'rgba(255,255,255,0.4)',
+                maxWidth: '360px',
               }}
-            >
-              Call Now
-            </Button>
+            />
           </AppearingAnimation>
+
+          <Stack direction={{ xs: 'row', sm: 'row' }} spacing={3} mt={5}>
+            <AppearingAnimation delay={1.1}>
+              <Button
+                variant='contained'
+                size='large'
+                sx={{
+                  px: 2,
+                  py: 1.5,
+                  fontWeight: 'bold',
+                  fontSize: { xs: '0.8rem', sm: '1rem', md: '1rem' },
+                }}
+              >
+                Request Service
+              </Button>
+            </AppearingAnimation>
+
+            <AppearingAnimation delay={1.2}>
+              <Button
+                variant='outlined'
+                component='a'
+                href='tel:+1234567890'
+                size='large'
+                sx={{
+                  color: '#fff',
+                  borderColor: '#fff',
+                  fontSize: { xs: '0.8rem', sm: '1rem', md: '1rem' },
+                  px: 2,
+                  py: 1.5,
+                  fontWeight: 'bold',
+                  '&:hover': {
+                    backgroundColor: 'rgba(255, 255, 255, 0.1)',
+                    borderColor: '#fff',
+                  },
+                }}
+              >
+                Call Now
+              </Button>
+            </AppearingAnimation>
           </Stack>
         </Box>
 
         <Box sx={{ position: 'relative' }}>
           <SwipingFromLeftAnimation delay={1.2}>
             <CardMedia
-              component="img"
+              component='img'
               sx={{
-                height: {md: 600, lg: 660}, 
-                left: {md: '-85px', lg: '-75px'},
-                top: {md: '-210px', lg: '-285px'},
-                width: "auto", 
-                objectFit: "contain", 
-                marginBottom: 1, 
+                height: { md: 570, lg: 590 },
+                left: { md: '-0px', lg: '100px' },
+                top: { md: '-280px', lg: '-285px' },
+                width: 'auto',
+                objectFit: 'contain',
+                marginBottom: 1,
                 position: 'absolute',
-                display: {xs: 'none', md: 'flex'}
+                display: { xs: 'none', md: 'flex' },
               }}
-              image='/hero-img/plumber-no-bg.png'
+              image='/hero-img/plumber-no-bg-3.png'
               alt='plumber'
             />
           </SwipingFromLeftAnimation>
@@ -91,7 +100,6 @@ const HeroSection = () => {
       </Box>
 
       <Waves />
-      
     </Box>
   );
 };
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import {
   AppBar,
   Toolbar,
@@ -12,12 +12,12 @@ import {
   ListItemButton,
   Box,
   Divider,
-} from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
-import SwipingAnimation from "../../UI/Animations/SwipingAnimation";
-import { navLinks } from "../../assets/headerData";
-import { headerContainerStyles, headerToolbarStyles } from "./headerStyles";
-import PhoneIcon from "@mui/icons-material/Phone";
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import SwipingAnimation from '../../UI/Animations/SwipingAnimation';
+import { navLinks } from '../../assets/headerData';
+import { headerContainerStyles, headerToolbarStyles } from './headerStyles';
+import PhoneIcon from '@mui/icons-material/Phone';
 
 const Header = () => {
   const [mobileOpen, setMobileOpen] = useState(false);
@@ -28,19 +28,24 @@ const Header = () => {
 
   return (
     <>
-      <AppBar
-        position="sticky"
-        sx={headerContainerStyles}>
+      <AppBar position='sticky' sx={headerContainerStyles}>
         <Toolbar sx={headerToolbarStyles}>
           {/* Logo */}
           <SwipingAnimation delay={0}>
-            <Box component="div" sx={{ display: 'flex', alignItems: 'center' }}>
-              <img 
-                src="/header-img/logo-1.svg" 
-                alt="Logo" 
+            <Box component='div' sx={{ display: 'flex', alignItems: 'center' }}>
+              <img
+                src='/header-img/logo-1.svg'
+                alt='Logo'
                 style={{ height: 30, marginRight: 10 }}
               />
-              <Typography variant="h6" component="div" sx={{ fontWeight: "bold", fontSize: { xs: '16px', lg: '20px' } }}>
+              <Typography
+                variant='h6'
+                component='div'
+                sx={{
+                  fontWeight: 'bold',
+                  fontSize: { xs: '16px', lg: '20px' },
+                }}
+              >
                 PLUMBING <span style={{ color: '#16a5e2' }}>SERVICES</span>
               </Typography>
             </Box>
@@ -50,123 +55,144 @@ const Header = () => {
           </SwipingAnimation>
 
           {/* Desktop Navigation */}
-          <Box sx={{ display: { xs: "none", md: "flex" }, gap: {md: 3, lg: 6}, alignItems: "center" }}>
-            <Box sx={{display: 'flex', gap: {md: 2, lg: 4}}}>
+          <Box
+            sx={{
+              display: { xs: 'none', md: 'flex' },
+              gap: { md: 3, lg: 6 },
+              alignItems: 'center',
+            }}
+          >
+            <Box sx={{ display: 'flex', gap: { md: 2, lg: 4 } }}>
               {navLinks.map((link) => (
-              <SwipingAnimation delay={link.delay}>
-                <Button
-                  key={link.title}
-                  component={Link}
-                  to={link.path}
-                  sx={{
-                    color: "white",
-                    textTransform: "none",
-                    fontWeight: 600,
-                    fontSize: { md: '12px', lg: '16px' }
-                  }}
-                >
-                  {link.title}
-                </Button>
-              </SwipingAnimation>
-            ))}
+                <SwipingAnimation delay={link.delay}>
+                  <Button
+                    key={link.title}
+                    component={Link}
+                    to={link.path}
+                    sx={{
+                      color: 'white',
+                      textTransform: 'none',
+                      fontWeight: 600,
+                      fontSize: { md: '12px', lg: '16px' },
+                    }}
+                  >
+                    {link.title}
+                  </Button>
+                </SwipingAnimation>
+              ))}
             </Box>
 
             {/* Contact info */}
             <SwipingAnimation delay={0.7}>
-              <Box
-                sx={{ textAlign: "right", }}
-              >
+              <Box sx={{ textAlign: 'right' }}>
                 <Typography
-                  variant="caption"
-                  display="block"
+                  variant='caption'
+                  display='block'
                   sx={{
                     opacity: 0.7,
-                    fontSize: { xs: '10px', sm: '12px', md: '11px', lg: '13px' }
-                  }}>
+                    fontSize: {
+                      xs: '10px',
+                      sm: '12px',
+                      md: '11px',
+                      lg: '13px',
+                    },
+                  }}
+                >
                   24/7 Emergency Call
                 </Typography>
                 <Typography
-                  component="a"
-                  href="tel:+1234567890"
-                  variant="subtitle1"
+                  component='a'
+                  href='tel:+1234567890'
+                  variant='subtitle1'
                   sx={{
-                    fontWeight: "bold",
-                    fontSize: { xs: '10px', sm: '12px', md: '16px', lg: '18px' },
+                    fontWeight: 'bold',
+                    fontSize: {
+                      xs: '10px',
+                      sm: '12px',
+                      md: '16px',
+                      lg: '18px',
+                    },
                     color: 'white',
                     textDecoration: 'none',
-                  }}>
+                  }}
+                >
                   123-456-7890
                 </Typography>
               </Box>
             </SwipingAnimation>
 
             <SwipingAnimation delay={0.9}>
-              <Button variant="contained" sx={{bgcolor: '#f27917'}} >
+              <Button variant='contained' sx={{ bgcolor: '#f27917' }}>
                 Get a Quote
               </Button>
             </SwipingAnimation>
           </Box>
 
           {/* Mobile Menu Button */}
-          <SwipingAnimation delay={0.2}>
-            <IconButton
-              color="inherit"
-              edge="end"
-              onClick={toggleDrawer}
-              sx={{ display: { xs: "block", md: "none" } }}
-            >
-              <MenuIcon />
+          <Box sx={{ display: { xs: 'block', md: 'none' } }}>
+            <SwipingAnimation delay={0.2}>
+              <IconButton color='inherit' edge='end' onClick={toggleDrawer}>
+                <MenuIcon />
               </IconButton>
-          </SwipingAnimation>
+            </SwipingAnimation>
+          </Box>
         </Toolbar>
       </AppBar>
 
       {/* Mobile Drawer */}
-      <Drawer anchor="right" open={mobileOpen} onClose={toggleDrawer}
+      <Drawer
+        anchor='right'
+        open={mobileOpen}
+        onClose={toggleDrawer}
         PaperProps={{
-    sx: {
-      bgcolor: "#0f172a", // тёмный фон
-      color: "#fff",      // цвет текста
-    },
-  }}>
-        <Box sx={{ width: 250, p: 2,  }}>
-          <Typography variant="h6" sx={{ mb: 2, fontWeight: "bold" }}>
+          sx: {
+            bgcolor: '#0f172a', // тёмный фон
+            color: '#fff', // цвет текста
+          },
+        }}
+      >
+        <Box sx={{ width: 250, p: 2 }}>
+          <Typography variant='h6' sx={{ mb: 2, fontWeight: 'bold' }}>
             Menu
           </Typography>
           <Divider />
           <List>
             {navLinks.map((link) => (
               <ListItem key={link.title} disablePadding>
-                <ListItemButton component={Link} to={link.path} onClick={toggleDrawer}>
+                <ListItemButton
+                  component={Link}
+                  to={link.path}
+                  onClick={toggleDrawer}
+                >
                   {link.title}
                 </ListItemButton>
               </ListItem>
             ))}
 
-            <Divider sx={{ my: 1.5, borderColor: "rgba(255,255,255,0.1)" }} />
+            <Divider sx={{ my: 1.5, borderColor: 'rgba(255,255,255,0.1)' }} />
 
             {/* Phone call button */}
             <ListItem disablePadding>
               <ListItemButton
-                component="a"
-                href="tel:+1234567890"
+                component='a'
+                href='tel:+1234567890'
                 sx={{
-                  color: "#fff",
+                  color: '#fff',
                   borderRadius: 1,
-                  fontWeight: "bold",
-                  "&:hover": {
-                    bgcolor: "rgba(255,255,255,0.12)",
+                  fontWeight: 'bold',
+                  '&:hover': {
+                    bgcolor: 'rgba(255,255,255,0.12)',
                   },
                 }}
               >
-                <PhoneIcon sx={{ mr: 1, color: "#f27917" }} />
+                <PhoneIcon sx={{ mr: 1, color: '#f27917' }} />
                 123-456-7890
               </ListItemButton>
             </ListItem>
           </List>
 
-          <Box sx={{ mt: 2, }}>
-            <Button variant="contained" sx={{bgcolor: '#f27917'}} fullWidth>
+          <Box sx={{ mt: 2 }}>
+            <Button variant='contained' sx={{ bgcolor: '#f27917' }} fullWidth>
               Get a Quote
             </Button>
           </Box>
